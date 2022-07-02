150c922bc916b077be919f7de5edcdab1dcfa61b
RujenMaharjan <rujenmazn@gmail.com>
2022-07-02 11:32:35 +0545

    update

---
+++ Assignments/Nested.py
@@ -0,0 +1,19 @@
+# a= int(input("enter your age"))
+# if a<=18:
+#     print("UNDERAGE")
+# else:
+#     if a>=25 and a<=40:
+#         print("VIP LOUNGE")
+#     if a>18 and a<25:
+#         print("NORMAL LOUNGE")
+#     else:
+#         print("OVER AGE TO ENTER")
+#         exit
+
+
+a=range(10,20)
+print(a)
+
+
+
+
---
+++ Loop/Nested.py
@@ -0,0 +1,56 @@
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i * j,end=' ')
+#     print()
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+
+# rows=5
+# for i in  range(1 , rows+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print('')
+
+
+# v=2
+# for i in range(1,6):
+#     for j in range(1,v):
+#         print(i,end=" ")
+#     v+=1
+#     print()
+
+
+# for x in range(0,5):
+#     for y in range(0,5):
+#         print("#",end=" ")
+#     print()
+
+
+
+# v=1
+# for x in range(0,5):
+#     for y in range(0,v):
+#         print('*',end="")
+#     v+=1
+#     print()
+
+
+# for x in range(5):
+#     for y in range(5-x):
+#         print('#',end=" ")
+#     print()
+
+
+
+# v=5
+# for x in range(5):
+#     for y in range(5-x):
+#         print(v, end=" ")
+#     v-=1
+#     print()
+
+
