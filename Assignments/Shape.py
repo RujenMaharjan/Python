92f6b05eee572ed628835b73a5b4f209dadba93f
RujenMaharjan <rujenmazn@gmail.com>
2022-07-02 12:06:16 +0545

    update

--- Assignments/Shape.py
+++ Assignments/Shape.py
@@ -1,3 +1,4 @@
+# Heart
 # for row in range(6):
 #     for column in range(7):
 #         if row==0 and column % 3!=0 or row==1 and column % 3==0 or row-column==2 or row+column==8:
@@ -7,3 +8,20 @@
 #     print()
 
 
+# Diamond
+# for x in range(4):
+#     for y in range(x,5):
+#         print(" ",end=" ")
+#     for y in range(x+1):
+#         print("*",end=" ")
+#     for y in range(x):
+#         print("*",end=" ")
+#     print()
+# for a in range(5):
+#     for b in range(a+1):
+#         print(" ",end=" ")
+#     for b in range(a,5):
+#         print("*",end=" ")
+#     for b in range(a,5-1):
+#         print("*",end=" ")
+#     print()
