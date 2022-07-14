8fbbc1902aa677033a964795b5cc9db21018e62c
RujenMaharjan <rujenmazn@gmail.com>
2022-07-14 11:48:27 +0545

    test

---
+++ Assignments/Nestedloop.py
@@ -0,0 +1,109 @@
+# a=int(input("Enter your age:"))
+# if a>18 and a<41:
+#     if a>=25 and a<=40:
+#         print("You are welcome to our vip lounge")
+#     else:
+#         print("you are welcome to our normal lounge")
+# else:
+#     print("Sorry you are not invited to the party")
+
+
+
+
+
+# v=int(input("enter your age:"))
+# if v<=18 :
+#     print("Underage")
+# else:
+#     if v>=25 and v<=40:
+#         print("VIP")
+#     elif v<25:
+#         print("Normal")
+#     else:
+#         print("Old")
+
+# b=int(input("enter any number:"))
+# if b%3==0:
+#     if b%5==0:
+#         print("It is completely divisible by both 5 and 3")
+#     else:
+#         print("It is divisible by 3 but not 5")
+# else:
+#
+#     if b%5==0:
+#         print("It is divisible by 5 but not 3")
+#     else:
+#         print("It is not divisible by 3 nor 5")
+
+
+# x=1
+#
+# while x<10:
+#     print(x)
+#     x+=1
+
+
+# x=1
+# while x<21:
+#     print("Hello Python")
+#     x+=1
+
+
+# x=1
+# even=0
+# odd=0
+# while x<11:
+#     if x%2==0:
+#         even+=1
+#         x+=1
+#     else:
+#         odd+=1
+#         x+=1
+#
+#
+# print("Total even=",even)
+# print("Total odd=",odd)
+
+# x=1
+# n=int(input("Enter number of name you want to print:"))
+# while x<=n:
+#     name=input("Enter a name:")
+#     print("Your Name is,",name)
+#     x+=1
+
+
+
+# x=1
+# n=int(input("How many students result do you want to enter:"))
+# while x<=n:
+#     for a in range(1):
+#         name=input("Enter a name:")
+#         phy=int(input("Enter marks of physics:"))
+#         chem=int(input("Enter marks of chemistry:"))
+#         bio=int(input("Enter marks of biology:"))
+#         maths=int(input("Enter marks of maths:"))
+#         comp=int(input("Enter marks of computer:"))
+#         avg=(phy+chem+bio+maths+comp)*(0.2)
+#         gpa=avg*(0.04)
+#         print("Your name is",name,end=" ")
+#         print("and you've obtained",str(gpa))
+#         if gpa<1.2:
+#             print("You've obtained E")
+#         elif gpa>=1.2 and gpa<=1.6:
+#             print("you've obtained D")
+#         elif gpa>=1.6 and gpa<=2.0:
+#             print("You've obtained C")
+#         elif gpa>=2.0 and gpa<=2.4:
+#             print("You've obtained C+")
+#         elif gpa>=2.4 and gpa<=2.8:
+#             print("You've obtained B")
+#         elif gpa>=2.8 and gpa<=3.2:
+#             print("You've obtained B+")
+#         elif gpa>=3.2 and gpa<=3.6:
+#             print("You've obtained A")
+#         elif gpa>=3.6 and gpa<=4.0:
+#             print("You've obtained A+")
+#         else:
+#             print("You've Entered wrong marks")
+#     x+=1
+
---
+++ Function/Def.py
@@ -0,0 +1,24 @@
+# def nepal():
+#     print("Nepal")
+#
+# nepal()
+
+# def name(firstname, secondname):
+#     print(firstname, secondname)
+#
+# name("Rujen","Maharjan")
+
+
+
+# def name():
+#     return [10,10,10]
+# def calc():
+#     a=name()
+#     p=a[0]
+#     t=a[1]
+#     r=a[2]
+#     return (p*t*r)/100
+# def disp():
+#     print(calc())
+#
+# disp()
